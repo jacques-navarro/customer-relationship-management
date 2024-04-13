e722bf4efac2632306aceb0607d49d33b98e8a65
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-04-13 21:56:50 +0200

    Initial commit

---
+++ src/main/java/dev/passingarguments/crm/CRMApplication.java
@@ -0,0 +1,11 @@
+package dev.passingarguments.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CRMApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CRMApplication.class, args);
+    }
+}
