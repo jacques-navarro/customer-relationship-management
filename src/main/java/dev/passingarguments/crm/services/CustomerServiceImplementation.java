565a969ba9eb46463a7f2b80b978178a934236db
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-04-15 12:42:50 +0200

    Add @Transactional annotation to addCustomer method

--- src/main/java/dev/passingarguments/crm/services/CustomerServiceImplementation.java
+++ src/main/java/dev/passingarguments/crm/services/CustomerServiceImplementation.java
@@ -4,6 +4,7 @@ import dev.passingarguments.crm.entities.Customer;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CustomerServiceImplementation implements CustomerService {
@@ -16,6 +17,7 @@ public class CustomerServiceImplementation implements CustomerService {
     }
 
     @Override
+    @Transactional
     public void addCustomer(Customer customer) {
         entityManager.persist(customer);
     }
