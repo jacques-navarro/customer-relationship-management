148bb923574920184e6da116288f4abe81028386
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-04-16 08:55:55 +0200

    Add display_sales_by_customer_id.html

--- src/main/java/dev/passingarguments/crm/controllers/SaleController.java
+++ src/main/java/dev/passingarguments/crm/controllers/SaleController.java
@@ -1,11 +1,13 @@
 package dev.passingarguments.crm.controllers;
 
 import dev.passingarguments.crm.entities.Sale;
+import dev.passingarguments.crm.repositories.SaleRepository;
 import dev.passingarguments.crm.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,10 +15,12 @@ import java.util.List;
 public class SaleController {
 
     private SaleService saleService;
+    private SaleRepository saleRepository;
 
     @Autowired
-    public SaleController(SaleService saleService) {
+    public SaleController(SaleService saleService, SaleRepository saleRepository) {
         this.saleService = saleService;
+        this.saleRepository = saleRepository;
     }
 
     @GetMapping("/sales")
@@ -24,5 +28,12 @@ public class SaleController {
         List<Sale> sales = saleService.findAll();
         model.addAttribute("sales", sales);
         return "display_sales";
+    }
+
+    @GetMapping("/sales/{customerId}")
+    public String findByCustomerId(@PathVariable("customerId") Long customerId, Model model) {
+        List<Sale> salesByCustomerId = saleRepository.findByCustomerId(customerId);
+        model.addAttribute("salesByCustomerId", salesByCustomerId);
+        return "display_sales_by_customer_id";
     }
 }
--- src/main/java/dev/passingarguments/crm/repositories/SaleRepository.java
+++ src/main/java/dev/passingarguments/crm/repositories/SaleRepository.java
@@ -3,6 +3,9 @@ package dev.passingarguments.crm.repositories;
 import dev.passingarguments.crm.entities.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    List<Sale> findByCustomerId(Long id);
 }
