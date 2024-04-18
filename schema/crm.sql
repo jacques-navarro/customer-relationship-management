3adb66150872d173f9e32ce7863ff02f77b3c47e
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-04-18 08:52:47 +0200

    Add code that creates 'sale' table to schema.sql

--- schema/crm.sql
+++ schema/crm.sql
@@ -14,5 +14,20 @@ CREATE TABLE customer (
 );
 
 INSERT INTO customer VALUES
+(1, "cn1", "em1", "pn1", "sn1", "sn1", 11),
 (2, "cn2", "em2", "pn2", "sn2", "sn2", 222),
 (3, "cn3", "em3", "pn3", "sn3", "sn3", 333);
+
+CREATE TABLE sale (
+  id BIGINT AUTO_INCREMENT NOT NULL,
+   product_name VARCHAR(50) NULL,
+   quantity_sold INT NULL,
+   transaction_date date NULL,
+   customer_id BIGINT NULL,
+   CONSTRAINT pk_sale PRIMARY KEY (id)
+);
+
+ALTER TABLE sale ADD CONSTRAINT FK_SALE_ON_CUSTOMER FOREIGN KEY (customer_id) REFERENCES customer (id);
+
+INSERT INTO sale VALUES
+(1, "product 1", 2, "2024-04-15", 2);
