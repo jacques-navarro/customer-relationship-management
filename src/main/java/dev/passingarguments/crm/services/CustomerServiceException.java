a85caaf34707c21addf786cd67cdf17e78012283
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2025-02-11 15:35:56 +0100

    Add CustomerServiceException

---
+++ src/main/java/dev/passingarguments/crm/services/CustomerServiceException.java
@@ -0,0 +1,9 @@
+package dev.passingarguments.crm.services;
+
+public class CustomerServiceException extends RuntimeException {
+
+    public CustomerServiceException(String message) {
+        super(message);
+    }
+
+}
